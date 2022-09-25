5e04c804d964426b5be95e8302f238017a6efb94
Karthick Raja <113981194+KarthickRajakr@users.noreply.github.com>
2022-09-26 00:03:37 +0530

    Create Program3.py
    Python program to find the prime numbers upto the given range.

---
+++ Assessments/Karthick Raja .M/Assignment 1/Program3.py
@@ -0,0 +1,21 @@
+n = int(input())
+primes = []
+
+for i in range(2, n + 1):
+    flag = 0
+
+    if i < 2:
+        continue
+    if i == 2:
+        primes.append(2)
+        continue
+
+    for x in range(2, i):
+        if i % x == 0:
+            flag = 1
+            break
+
+    if flag == 0:
+        primes.append(i)
+        
+print(primes)
