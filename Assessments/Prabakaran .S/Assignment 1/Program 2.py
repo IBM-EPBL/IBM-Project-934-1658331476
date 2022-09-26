bea785eaf4901957848c4e10d97159a2b013fe12
Prabakaran S <113983701+Prabakarans11@users.noreply.github.com>
2022-09-26 20:38:30 +0530

    Update Program 2.py

--- Assessments/Prabakaran .S/Assignment 1/Program 2.py
+++ Assessments/Prabakaran .S/Assignment 1/Program 2.py
@@ -1,5 +1,10 @@
 a=int(input('Enter the start'))
 b=int(input('Enter the end'))
 for i in range(a+1,b):
+i=a
+while True :
+    i=i+1
+    if(i>=b):
+        break
     if(i%2!=0):
         print(i)
