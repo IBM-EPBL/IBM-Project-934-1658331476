0cbbc1593f107c5f422106ada63a67d5942bf38e
Karthick Raja <113981194+KarthickRajakr@users.noreply.github.com>
2022-11-19 21:29:18 +0530

    Delete Final Deliverables/Documentaion/Final Code directory

--- Final Deliverables/Documentaion/Final Code/Backend/js/bookmarks.js
+++
@@ -1,6 +0,0 @@
-// let bookmarks = document.querySelectorAll(".bookmark");
-// bookmarks.forEach( bookmark => {
-//     bookmark.addEventListener("click", () => {
-//         EventTarg
-//     })
-// })
